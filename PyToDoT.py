b9b2ab576ae69b7e47f624b551b4d5ef831fee40
Yale Winter <yalemwinter@gmail.com>
2022-07-19 10:24:59 -0700

    update fix date columns , remove load online

--- PyToDoT.py
+++ PyToDoT.py
@@ -21,9 +21,6 @@ etc ...
 
 To-Do Priority must be greater than 0 to show up when finding next To-Dos
 
-To load your live google sheet online (set so anyone with the link can view):
-Change import_online to True, and replace ___online_url___ with that part of your url
-
 To load your offline .csv:
 Download your To-Dos as .csv (only downloading selected collumns and rows)
 And name the document 'ToDos.csv' and place in the same folder
@@ -36,6 +33,7 @@ Run command "to_do(1, df)" in the console to see the full text Description of To
 
 import pandas as pd
 from datetime import date, timedelta
+import os
 
 
 def to_do(x, df):
@@ -51,10 +49,10 @@ def find_recent_progress(df, prog):
     '''
     days_before = pd.Timestamp((date.today()-timedelta(days=30)))
     df3 = df.iloc[0,1]
-    delta = pd.Timestamp(date.today()) - df3
+    delta = pd.Timestamp(date.today()) - pd.Timestamp(df3)
     print('Days since tracking:',delta.days)
     print('Average To-Dos completed in 30 days:', int(prog[0] / delta.days * 30))
-    df2 = df.loc[df['Date Complete'] > days_before,]
+    df2 = df.loc[df['Date Complete'] > str(days_before),]
     print('To-Dos completed in last 30 days:',len(df2))
     
 def find_to_do_progress(df):
@@ -90,13 +88,20 @@ def fix_dates_in_col(df, col_index, col_names):
     modified DataFrame
 
     '''
+
     ldf = df.values.tolist()
-    for i in range(len(ldf)):
-        ldf[i][col_index] = pd.to_datetime(df.iloc[i][col_index])
+    for i in range(1, len(ldf)):
+        try:
+            r = pd.to_datetime(df.iloc[i][col_index], errors='coerce')
+            r = r[:10]
+            ldf[i][col_index] = r
+        except:
+            pass
     df = pd.DataFrame(ldf, columns=col_names)  
+
     return df
 
-def import_data_table(file_name, online, gsheet_mid_link, read_rows, col_names):
+def import_data_table(file_name, read_rows, col_names):
     '''
     Import timeline from .csv file
     
@@ -117,44 +122,35 @@ def import_data_table(file_name, online, gsheet_mid_link, read_rows, col_names):
 
     '''
     df = 'error importing data'
-    if online:
-        try:
-            df = pd.read_csv('https://docs.google.com/spreadsheets/d/' + 
-            gsheet_mid_link +
-            '/export?gid=0&format=csv',nrows=read_rows, on_bad_lines='skip')
-            print('loaded data table from google sheet online')
-        except:
-            print('error loading data table from google sheet online')
-            online = False
-            
-    if online == False:
-        try:
-            df = pd.read_csv(file_name,nrows=read_rows, on_bad_lines='skip')
-            print('loaded data table from local .csv')
-        except:
-            print('error loading data from local .csv')
+    try:
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        sheet_url = os.path.join(__location__, file_name)  
+        df = pd.read_csv(sheet_url,nrows=read_rows, on_bad_lines='skip')
+        print('loaded data table from local .csv')
+    except:
+        print('error loading data from local .csv')
     
     # drop rows where at least 1 element is missing
     if type(df) == pd.DataFrame:
         df.dropna()
-
     return df
 
 def start():
     '''
     Start up and display To-Do information
     '''
-    import_online = False
-    gsheet_mid_link = 'your_url_here'
     col_names = ['To-Dos', 'Date Assigned', 'Date Complete', 'Priority']
-    df = import_data_table('ToDos.csv', import_online, gsheet_mid_link, 1000, col_names)
+    df = import_data_table('ToDos.csv', 1000, col_names)
     df = fix_dates_in_col(df, 1, col_names)
     df = fix_dates_in_col(df, 2, col_names)
     find_next_to_dos(df)
     prog = find_to_do_progress(df)
     find_recent_progress(df, prog)
+
+    return df
     
-start()
+df = start()
 
 
 
